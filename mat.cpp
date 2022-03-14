237dc605963060bb47d0bdcc012bbc31b0d893f1
Ori Darshan <oridarshan@gmail.com>
2022-03-14 10:02:37 +0200

    add comments

--- mat.cpp
+++ mat.cpp
@@ -19,67 +19,42 @@ string ariel::mat(int col, int row, char c1, char c2){
         throw ("characters must be printable");
     }
 
-    // initialize mat
-    // char arr[row][col+1];
-    // std::array<std::array<char, row>, col+1> arr;
+    // initialize mat as vetor of vectors of chars
     vector<vector<char>> arr (row, vector<char>(col+1));
+
+    // add end of line for each row
     for (int i = 0; i < row; i++)
     {
         arr[i][col] = '\n';
     }
+
     char c = c2;
+    // loop over edge sizes
     for (int k = 0; k < max(row,col); k++){
+        // switch symbol for each iteration
         c = (c == c2) ? c1 : c2;
         for (int i = k; i < row - k; i++)
+        // paint sub-mat with relevant symbol
         {
             for (int j = k; j < col - k; j++)
             {
                 arr[i][j] = c;
-            }
-            
+            }         
         }
-        
-        // paint upper and lower rows
-        // for (int i = 0 + k; i < col - k; i++)
-        // {
-        //     cout << "fill " << k << i << endl;
-        //     arr[k][i] = c;
-        //     cout << "fill " << row - 1 - k << i << endl;
-        //     arr[row - 1 - k][i] = c;
-            
-        // }
-        // // paint right and left columns
-        // for (int i = 0 + k; i < row - k; i++)
-        // {
-        //     // arr[i][k] = c;
-        //     cout << "fill " << i << k << endl;
-        //     arr.at(i).at(k) = c;
-        //     cout << "fill [" << i << "][" << col - 1 - k << "]" << endl;
-        //     arr[i][col - 1 - k] = c;
-        // }
-        // arr[i][j] = '\n';        // cout << arr[i][j];
-        
     }
-    // print mat
-
+    // create result string
     string res;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col+1; j++)
         {
-            //cout << arr[i][j];
+            // append symbol from mat to result string
             res.push_back(arr[i][j]);
+
+            //cout << arr[i][j]; //debuging
         }
         
     }
-    
-
-    // string res = "";
-    // for (int i = 0; i < row; i++)
-    // {
-    //     res.append((string)arr[i][0]);
-    // }
-    
     
     return res;
 }
